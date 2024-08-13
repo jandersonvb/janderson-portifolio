d1b21901a4093d175609e7b7c8df35a1f424765e
jandersonvb <jandersonvb.dev@gmail.com>
2024-08-12 22:57:19 -0300

    feat: configure next seo

---
+++ next-seo.config.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Janderson Vilas Boas',
+  description: 'Portfólio de Janderson Vilas Boas',
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://jandersonvilasboas.com.br/',
+    site_name: 'Janderson Vilas Boas',
+  },
+  images: [
+    {
+      url: 'https://jandersonvilasboas.com.br/assets/photo.png',
+      width: 1200,
+      height: 630,
+      alt: 'Janderson Vilas Boas',
+    },
+  ],
+  twitter: {
+    handle: '@jandersonvb_dev',
+    site: '@jandersonvb_dev',
+    cardType: 'summary_large_image',
+  },
+}
